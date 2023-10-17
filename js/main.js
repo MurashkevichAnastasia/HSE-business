5d4ff93afd8ea7880b65751b23ed58d963e4c2a3
MurashkevichAnastasia <113125758+MurashkevichAnastasia@users.noreply.github.com>
2023-10-17 15:56:52 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// ANIMATION ON TELEGRAM HOVER
+function hoverOnTelegram() {
+  document.getElementById("telegram").classList.add("active");
+}
+function hoverOffTelegram() {
+  document.getElementById("telegram").classList.remove("active");
+}
+
+// ANIMATION ON VK HOVER
+function hoverOnVk() {
+  document.getElementById("vk").classList.add("active");
+}
+function hoverOffVk() {
+  document.getElementById("vk").classList.remove("active");
+}
